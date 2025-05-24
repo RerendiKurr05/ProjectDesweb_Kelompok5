1de29fef27a9685e3a064fc66e0ad0f71abd9789
Dndrskyy <dindariski1246@gmail.com>
2025-05-24 16:11:25 +0700

    Update_Dinda
    Keranjang ketika di klik ada pembayaran Tunai atau QRIS ketika selesai akan diarahkan ke Whatsaap dengan menampilkan nama, jam diambil, dan total harga barang yang dipilih. Berlaku juga untuk di Index. Trus untuk index tampilan barang best kuhapus, trus about-us setelah ngisi langsung diarahkan ke nomernya saskia
    
    Co-Authored-By: RerendiKurr05 <renistooshort@gmail.com>

--- ATK Fatimah/ATK Fatimah/src/js/contact-us.js
+++ ATK Fatimah/ATK Fatimah/src/js/contact-us.js
@@ -2,7 +2,7 @@ document.querySelector('button.submit').addEventListener('click', function() {
     var name = encodeURIComponent(document.querySelector('.name input').value);
     var email = encodeURIComponent(document.querySelector('.email input').value);
     var text = encodeURIComponent(document.querySelector('.msg textarea').value);
-    var message = `Hallo, Toko ATK Fatimah! Saya ${name}.%0A%0A${text}%0A%0ARegards, ${name}%0A%0A${email}`;
-    var url = `https://api.whatsapp.com/send?phone=6285606058061&text=${message}`;
+    var message = `Hallo, Toko ATK Fatimah! Saya ${name}.%0A%0A${text}%0A%0A, ${name}%0A%0A${email}`;
+    var url = `https://api.whatsapp.com/send?phone=6283833349662&text=${message}`;
     window.open(url);
   });
